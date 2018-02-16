6fd2aefa26d87598ff1829f36debf6ad67484312
DawidStefaniak <contact@dawidstefaniak.com>
2018-02-16 16:24:50 +0000

    Views for iOS added with logic to move between views

--- VictimApplication.Core/ViewModels/PoliceDetailsViewModel.cs
+++ VictimApplication.Core/ViewModels/PoliceDetailsViewModel.cs
@@ -6,6 +6,7 @@ namespace VictimApplication.Core.ViewModels
     public class PoliceDetailsViewModel : MvxViewModel
     {
         private string _details = "Your Policeman Details will be here";
+
         public string Details
         {
             get => _details;
---
+++ VictimApplication.iOS/Views/PoliceDetailsView.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using UIKit;
+
+namespace VictimApplication.iOS.Views
+{
+    public partial class PoliceDetailsView : UIViewController
+    {
+        public PoliceDetailsView() : base("PoliceDetailsView", null)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
+
